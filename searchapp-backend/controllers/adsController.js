d0b801223028336628d45aec7daa37b9df3889e2
surabhiweb <60553281+surabhiweb@users.noreply.github.com>
2022-10-31 01:10:07 +0530

    Update adsController.js

--- searchapp-backend/controllers/adsController.js
+++ searchapp-backend/controllers/adsController.js
@@ -1,49 +1,56 @@
 const path = require("path");
-const fs = require("fs");
-
-
 const ads = require("../models/ads");
 const company = require("../models/company");
 
 exports.getAds = ( req, res,next) => {
+  const searchFor = req.params.searchFor;
 
-  // console.log(req);
-   const searchFor = req.params.searchFor;
-  // ads.find({ $text: { $search : searchFor , $caseSensitive: false} }
-  // // , (err, response, res) => {
-  // //   console.log(err);
-  // //   console.log(response);
-  // //   console.log(res);
-  // // }
-  // )
-  ads.aggregate([  
-    {
-      $match: {
-      $text: { $search : searchFor, $caseSensitive : false }
-    },
+  
+  company.aggregate([{
+    "$lookup": {
+      "from": "ads",
+      "localField": "_id",
+      "foreignField": "companyId",
+      "as": "a"
+    }
+  },{
+    "$unwind": {
+      "path": "$a",
+      "preserveNullAndEmptyArrays": true
+    }
   },
-
-    {
-      $lookup:{
-        from: "companies",  // parent table
-        localField: "companyId",  // in child table
-        foreignField: "_id",
-        as: "companyDetails" // output as
-      }
-    },
-    {$unwind: { path:"$companyDetails" }},
-    {$project: {
-      "companyId":0,
-      "primaryText":0,
-      "headline":0,
-      "description":0,
-      "CTA":0,
-      "__v":0,
-      "_id":0,
-      "companyDetails._id":0,
-      "companyDetails.__v":0
-    }},
-  ])
+  {
+    "$match": {
+      "$or": [
+        {
+          "name": {
+            "$regex": "(?i)^.*"+searchFor+".*$" 
+          }
+        },
+        {
+          "a.primaryText": {
+            "$regex": "(?i)^.*"+searchFor+".*$" 
+          }
+        },
+        {
+          "a.headline": {
+            "$regex": "(?i)^.*"+searchFor+".*$" 
+          }
+        },
+        {
+          "a.description": {
+            "$regex": "(?i)^.*"+searchFor+".*$" 
+          }
+        },
+        {
+          "a.CTA": {
+            "$regex": "(?i)^.*"+searchFor+".*$" 
+          }
+        }
+      ]
+    }
+  }
+])
   .then((response) => {
     if (!response) {
       const error = new Error(
@@ -62,23 +69,7 @@ exports.getAds = ( req, res,next) => {
     if (!err.statusCode) err.statusCode = 500;
       next(err);
   });
-  // ads.findOne({})
-  //   .then((ads) => {
-  //     if (!ads) {
-  //       const error = new Error(
-  //         "Could not find any ads with the given searchText"
-  //       );
-  //       error.statusCode = 404;
-  //       throw error;
-  //     }
-  //     res
-  //       .status(200)
-  //       .json({ message: "Ads fetched successfully", ads: ads });
-  //   })
-  //   .catch((err) => {
-  //     if (!err.statusCode) err.statusCode = 500;
-  //     next(err);
-  //   });
+ 
 };
 
 
